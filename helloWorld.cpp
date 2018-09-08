7c1534f1d6238aaecc1ed403749e60ed060c0014
LEI XIA <thomaslei1995@gmail.com>
2018-09-08 16:45:44 -0400

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+
+
+// a small C++ program
+#include <iostream>
+
+int main(){
+	std::cout << "Hello, world!" << std::endl;
+	std::cout << "This (\") is a quote and this (\\) is a backslash." << std::endl;
+	return 0;
+}
+
