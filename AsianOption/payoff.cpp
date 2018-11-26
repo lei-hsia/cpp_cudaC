e5420fc636d05e522b90cefcec54e77ac5008c6e
LEI XIA <thomaslei1995@gmail.com>
2018-11-25 22:08:15 -0500

    Update payoff.cpp

--- AsianOption/payoff.cpp
+++ AsianOption/payoff.cpp
@@ -18,14 +18,14 @@ PayOff::PayOff() {}
 PayOffCall::PayOffCall(const double& K) { K = _K; }
 
 // Over-ridden operator() method, which turns PayOffCall into a function object
-// 这里相当于世第二重重载,因为header file中已经有PayOffCall/Put重载了
+// 这里相当于是第二重重载,因为header file中已经有PayOffCall/Put重载了
 // PayOff中的PayOff, 所以这里继续重载PayOffCall中的operator是第二重重载
 double PayOffCall::operator() (const double& S) const {
 	return std::max(S-K, 0.0);  // Euro Call
 }
 
 //  PayOffPut
-PayOffCall::PayOffPut(const double& _K) { K = _K; }
+PayOffPut::PayOffPut(const double& _K) { K = _K; }
 
 // 重载operator() method, which turns PayOffPut into a function object
 double PayOffPut::operator() (const double& S) const {
